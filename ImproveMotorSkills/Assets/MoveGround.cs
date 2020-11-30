8bd84374382e097c0ee941a5fb347e59e28f3c0b
Thair <Tahir2.Serali@live.uwe.ac.uk>
2020-11-30 01:47:26 +0000

    add begging part of moving platforms

---
+++ ImproveMotorSkills/Assets/MoveGround.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveGround : MonoBehaviour
+{
+    private float speed = 10;
+
+    public Transform transportpoint;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3 ( speed, 0, 0);
+
+        this.transform.position -= move * Time.deltaTime;
+
+        if(transform.position.x <= -21)
+        {
+            move = new Vector3(5, 0, 0);
+
+            this.transform.position = move;
+        }
+    }
+}
